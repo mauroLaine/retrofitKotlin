1d0b82788058812bf96e965928fbd665f23b13dd
Mauricio Laine <laine.mau@gmail.com>
2019-08-15 07:58:27 -0700

    created a HTTP request call using a Retrofit object

--- app/src/main/java/com/laine/mauro/retrofitv1/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/retrofitv1/MainActivity.kt
@@ -2,11 +2,40 @@ package com.laine.mauro.retrofitv1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.internal.GsonBuildConfig
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        load_button.setOnClickListener {
+
+            Toast.makeText(this, "hi!", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+    fun getData() {
+        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+
+    }
+
+    companion object {
+        val API_URL = "https://api.github.com/"
     }
 }
